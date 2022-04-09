a3966de7b80c4f6dc946c505c8ee7cd2b16a12cb
Ziad-d <ziadmo62@gmail.com>
2022-04-09 02:47:15 +0200

    deploy

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
 cd www
-eb init udagram-api --platform node.js --region us-east-1
+eb init udagram-api --platform node.js --region us-east-1 --platform "Node.js 16 running on 64bit Amazon Linux 2"
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET JWT_SECRET=$JWT_SECRET
 eb deploy env-name
